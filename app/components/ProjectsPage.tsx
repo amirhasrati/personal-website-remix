3ae1069aaeff7bb7fe9a4b2917dc2827e09fa12b
Amir Hasrati <ahasrati@uoguelph.ca>
2024-02-24 21:28:11 -0500

    Added _blank target to some buttons that were missing them so the link opens in new tab

--- app/components/ProjectsPage.tsx
+++ app/components/ProjectsPage.tsx
@@ -33,6 +33,7 @@ export default function ProjectsPage() {
                         </p>
                         <a
                             href="https://github.com/amirhasrati/todulo-app"
+                            target="_blank"
                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Github Page
                             <svg
